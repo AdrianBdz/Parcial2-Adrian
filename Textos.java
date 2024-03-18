2d6fc854c054c9a46dc715c59fe5a0cae25cea4e
AdrianBdz <162131145+AdrianBdz@users.noreply.github.com>
2024-03-18 11:22:37 +0100

    Creada clase textos
    Refactorizada y añadido el javadoc

---
+++ Textos.java
@@ -0,0 +1,14 @@
+/**
+ * Clase que contiene los textos fijos utilizados en la aplicación.
+ */
+public class Textos {
+    /**
+     * Mensaje para solicitar el porcentaje de aumento de salario.
+     */
+    public static final String MENSAJE_PORCENTAJE_AUMENTO = "Introduzca el porcentaje de aumento de salario: ";
+    
+    /**
+     * Mensaje para mostrar la lista de empleados.
+     */
+    public static final String MENSAJE_LISTA_EMPLEADOS = "Lista de Empleados:";
+}
